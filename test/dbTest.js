bf8edf335b7104663c3f5da10b9eda860ff9a554
alaabadra <alaabadra4@gmail.com>
2019-03-19 13:02:57 +0200

    create database and modified in test files

--- test/dbTest.js
+++ test/dbTest.js
@@ -1,10 +1,8 @@
 const test = require('tape');
 const DBRun = require('../src/database/build.js');
 
-test('testing the tape', assert=>{
-    const num = 1;
-      assert.equal(num, 1, "pass");
-      assert.end();
-  });
-  module.exports = DBRun;
-  
+test('testing the tape', (assert) => {
+  const num = 1;
+  assert.equal(num, 1, 'pass');
+  assert.end();
+});
