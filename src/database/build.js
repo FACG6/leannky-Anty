a641374b5a9d3b004683bcace542b78e6e84e567
AymanAlQoqa <aalqouqa@gmail.com>
2019-03-20 10:10:37 +0200

    try to resolve travis failier

--- src/database/build.js
+++ src/database/build.js
@@ -1,6 +1,6 @@
-const connect = require('./connection');
 const { readFileSync } = require('fs');
 const path = require('path');
+const connect = require('./connection');
 
 const sql = readFileSync(path.join(__dirname, 'build.sql')).toString();
 const DBRun = (file, cb) => {
