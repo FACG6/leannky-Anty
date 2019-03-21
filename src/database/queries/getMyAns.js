4ca8b9484b458b2091359d194eccc4c9f4c9b74e
AymanAlQoqa <aalqouqa@gmail.com>
2019-03-21 14:09:38 +0200

    resolve queries problems

--- src/database/queries/getMyAns.js
+++ src/database/queries/getMyAns.js
@@ -1,3 +1,5 @@
 const connection = require('../connection');
-const getMyAns = (consultant_id) => connection.query(`select * from posts where consultant_id=id`, [consultant_id]);
+
+const getMyAns = consultantId => connection.query('select * from posts where consultant_id=$1', [consultantId]);
+
 module.exports = getMyAns;
