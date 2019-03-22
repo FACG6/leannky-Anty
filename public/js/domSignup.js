cc670168d63f0bc33cd10691e9a721044ba8b332
AymanAlQoqa <aalqouqa@gmail.com>
2019-03-23 00:46:58 +0200

    login and signup styling relates#69

--- public/js/domSignup.js
+++ public/js/domSignup.js
@@ -4,27 +4,59 @@ const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirmPassword');
 const msgField = document.querySelector('.msg');
 
+const validate = (userName, password, confirmPassword) => {
+  if (!userName|| !password|| !confirmPassword) {
+    msgField.textContent = 'يجب تعبئة جميع الحقول';
+    return false;
+  }
+  if (userName.length < 4 || userName.length > 20) {
+    msgField.textContent = 'اسم المستخدم يجب ألا يزيد عن 20 وألا يقل عن 4 أحرف';
+    return false;
+  }
+  if (userName.includes(' ')) {
+    msgField.textContent = 'اسم المستخدم يجب ألا يحتوى على فراغات';
+    return false;
+  }
+  if (password.length < 3) {
+    msgField.textContent = 'كلمة المرور يجب ألا يقل عن 3 أحرف/أرقام';
+    return false;
+  }
+  if (password.includes(' ')) {
+    msgField.textContent = 'كلمة المرور لا تحتوى على فراغات';
+    return false;
+  }
+  if (password !== confirmPassword) {
+    msgField.textContent = 'يجب تطابق كلمتي المرور';
+    return false;
+  }
+  return true;
+};
 submitBtn.addEventListener('click', (event) => {
   event.preventDefault();
   const data = {
-    userName: userName.value,
-    password: password.value,
-    confirmPassword: confirmPassword.value,
+    userName: userName.value.trim(),
+    password: password.value.trim(),
+    confirmPassword: confirmPassword.value.trim(),
   };
-  fetch('/signup', {
-    method: 'POST',
-    body: JSON.stringify(data),
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(res => res.json())
-    .then((res) => {
-      if (res.msg) {
-        msgField.textContent = res.msg;
-      } else {
-        msgField.textContent = '';
-      }
-    }).catch((err) => { msgField.textContent = err; });
+  if (validate(data.userName, data.password, data.confirmPassword)) {
+    fetch('/signup', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then((res) => {
+        if (res.msg) {
+          msgField.textContent = res.msg;
+        } else {
+          msgField.textContent = '';
+        }
+      }).catch((err) => { msgField.textContent = err; });
+  }
 });
+userName.addEventListener('input', () => msgField.textContent = '');
+password.addEventListener('input', () => msgField.textContent = '');
+confirmPassword.addEventListener('input', () =>  msgField.textContent = '');
