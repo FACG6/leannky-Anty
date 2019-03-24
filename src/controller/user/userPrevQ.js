da0f6b1d7921e75b304eb1b1f062da048fb73f8a
alaabadra <alaabadra4@gmail.com>
2019-03-24 10:04:56 +0300

    create design userPrevQ

--- src/controller/user/userPrevQ.js
+++ src/controller/user/userPrevQ.js
@@ -2,6 +2,9 @@ exports.get = (req, res) => {
     if (req.token) {
         res.render('userPrevQ', {
             layout: 'user',
+            title : 'عنوان الاستشارة المدخلة',
+            subject : 'موضوع الاستشارة المدخلة',
+            answer : 'الاجابة',
             js: ['domUser'],
             css: 'user',
             username: req.token.userName,
