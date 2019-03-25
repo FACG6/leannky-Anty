93ce5d7e8798e660136b77d2f1d2971089f65ab4
Nareman <naremanmohhilles@gmail.com>
2019-03-25 23:50:00 +0200

    final edit css style

--- src/controller/user/userPrevQ.js
+++ src/controller/user/userPrevQ.js
@@ -2,15 +2,12 @@ exports.get = (req, res) => {
   if (req.token) {
     res.render('userPrevQ', {
       layout: 'user',
-      title : 'عنوان الاستشارة المدخلة',
-      subject : 'موضوع الاستشارة المدخلة',
-      answer : 'الاجابة',
       js: ['domUser'],
       css: 'user',
-      username: req.token.userName,
-      result: req.result
-        });
-    }else{
-        res.redirect('/');
-    }
+      userName: req.token.userName,
+      result: req.result,
+    });
+  } else {
+    res.redirect('/');
+  }
 };
