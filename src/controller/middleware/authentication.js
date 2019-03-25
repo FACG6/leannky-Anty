c22e8fe0c55a5c5cf3c1587e61733f86fee37d32
AymanAlQoqa <aalqouqa@gmail.com>
2019-03-25 15:25:16 +0300

    just one test to insure that there is not any problem with travis relates#PR#79

--- src/controller/middleware/authentication.js
+++ src/controller/middleware/authentication.js
@@ -2,8 +2,8 @@ exports.auth = (req, res, next) => {
   if (!req.token) {
     next();
   } else if (req.token && req.token.consultant) {
-    return res.render('consProfile');
+    return res.redirect('/consProfile');
   } else if (req.token && !req.token.consultant) {
-    return res.render('userProfile');
+    return res.redirect('userProfile');
   }
 };
--- test/test.js
+++ test/test.js
@@ -1,7 +1,27 @@
 const test = require('tape');
+const request = require('supertest');
+const app = require('../src/app');
+const dbRun = require('../src/database/build');
 
-test('testing the tape', (assert) => {
-  const num = 1;
-  assert.equal(num, 1, 'pass');
-  assert.end();
+test('Request signup route ', (t) => {
+  dbRun((err) => {
+    if (err) t.error('error', err);
+    request(app)
+      .post('/signup')
+      .send({
+        userName: 'ssadrgvd',
+        password: '123456',
+        confirmPassword: '123456',
+      })
+      .expect(200)
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .end((errr, res) => {
+        if (err) {
+          t.error(errr);
+        } else {
+          t.equal(JSON.parse(res.text).msg, 'تم انشاء الحساب بنجاح، يمكنكي الآن تسجيل الدخول', 'must return msg of sucsses signup');
+          t.end();
+        }
+      });
+  });
 });
