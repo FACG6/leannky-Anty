9cfbb85d8caa2370d63f8976e0b0dc2ddb2e76e1
Nareman <naremanmohhilles@gmail.com>
2019-03-27 10:39:50 +0200

    edit css style of user page

--- src/controller/user/userPrevQ.js
+++ src/controller/user/userPrevQ.js
@@ -2,15 +2,13 @@ exports.get = (req, res) => {
   if (req.token) {
     res.render('userPrevQ', {
       layout: 'user',
-      title: 'عنوان الاستشارة المدخلة',
-      subject: 'موضوع الاستشارة المدخلة',
-      answer: 'الاجابة',
       js: ['domUser'],
       css: 'user',
-      username: req.token.userName,
+      userName: req.token.userName,
       result: req.result,
     });
   } else {
     res.redirect('/');
   }
 };
+
