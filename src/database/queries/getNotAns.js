d4846218d77a457c50067ac3819c49af45946cfa
AymanAlQoqa <aalqouqa@gmail.com>
2019-03-28 10:15:07 +0300

    sorting posts desc

--- src/database/queries/getNotAns.js
+++ src/database/queries/getNotAns.js
@@ -1,5 +1,5 @@
 const connection = require('../connection.js');
 
-const getNotAns = () => connection.query('select * from posts where answer is null');
+const getNotAns = () => connection.query('select * from posts where answer is null ORDER BY id DESC');
 
 module.exports = getNotAns;
